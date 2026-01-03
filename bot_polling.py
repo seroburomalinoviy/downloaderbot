6c53dbbc5662ee7384b2e2278052d7c7120d9b13
grideperlevy <mr.ivandmi@yandex.ru>
2026-01-03 14:49:55 +0300

    update formats

--- bot_polling.py
+++ bot_polling.py
@@ -22,7 +22,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 YTDL_OPTS = {
-    "format": "bestvideo*+bestaudio/best",
+    "format": "bv*+ba/b",
     "merge_output_format": "mp4",
     "outtmpl": "%(id)s.%(ext)s",
     "quiet": True,
